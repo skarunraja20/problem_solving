7c799add2f855a2505708359e989f2099d44518f
skarunraja20 <skarunraja@gmail.com>
2024-12-04 10:43:22 +0530

    committing more commits

--- src/com/example/arrays/Reverse_an_array_inplace.java
+++ src/com/example/arrays/Reverse_an_array_inplace.java
@@ -1,20 +1,26 @@
 package com.example.arrays;
 
+import java.util.Arrays;
+
 public class Reverse_an_array_inplace {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int[] A = {1,2,3,4,5};
 		reverse(A);
+		Arrays.stream(A).forEach(System.out::print);
 		
 	}
 
 	private static void reverse(int[] A) {
 		int i = 0;
-		int j = A.length;
-		
-		while() {
-			
+		int j = A.length-1;
+		int  temp = 0;
+		while(i < j) {
+			temp = A[i];
+			A[i] = A[j];
+			A[j] = temp;
+			i++;
+			j--;
 		}
 	}
 	
