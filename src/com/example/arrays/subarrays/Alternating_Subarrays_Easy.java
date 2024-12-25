cdc3b96b7e670adfee2f58ee77ac6084c6bb5879
skarunraja20 <skarunraja@gmail.com>
2024-12-25 07:46:15 +0530

    2d arrays

--- src/com/example/arrays/subarrays/Alternating_Subarrays_Easy.java
+++ src/com/example/arrays/subarrays/Alternating_Subarrays_Easy.java
@@ -1,17 +1,53 @@
 package com.example.arrays.subarrays;
 
+import java.util.ArrayList;
+
 public class Alternating_Subarrays_Easy {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] A = {0,0,0,1,0,0,0,1,0,1,1};
+		int B = 1;
+		Alternating_Subarrays_Easy a = new Alternating_Subarrays_Easy();
+		int[] res = a.solve(A, B);
+		for (int i = 0; i < res.length; i++) {
+			System.out.print(res[i]+" ");
+		}
 	}
-	
+
 	public int[] solve(int[] A, int B) {
 		int subArrLen = (2*B)+1;
-		for(int s=0, e = subArrLen-1; e<A.length; s++,e++) {
-			
+		boolean isAltArr = false;
+		ArrayList<Integer> resList = new ArrayList<Integer>();
+		int start = 0;
+		int end = subArrLen-1;
+
+		while(end < A.length) {
+			isAltArr = isAlternatingArr(A, start, end);
+			if(isAltArr) {
+				resList.add((start + end) / 2);
+			}
+
+			start++;
+			end++;
 		}
-    }
+		return copyListToArray(resList);
+	}
+
+	private boolean isAlternatingArr(int[] A, int start, int end) {
+		for (int j = start; j < end; j++) {
+			if(A[j] == A[j+1])
+				return false;
+		}
+		return true;
+	}
+
+	private int[] copyListToArray(ArrayList<Integer> resList) {
+		int[] result = new int[resList.size()];
+		for (int i = 0; i < resList.size(); i++) {
+			result[i] = resList.get(i);
+		}
+		return result;
+	}
 
 }
--- src/com/example/arrays/twodimarrays/Matrix_Multiplication.java
+++ src/com/example/arrays/twodimarrays/Matrix_Multiplication.java
@@ -8,6 +8,27 @@ public class Matrix_Multiplication {
 	}
 	
 	public int[][] solve(int[][] A, int[][] B) {
+		int firstArrRowLen = A.length;
+		int firstArrColLen = A[0].length;
+		
+		int secondArrRowLen = A[0].length;
+		int secondArrColLen = B[0].length;
+		
+		for (int i = 0; i < A.length; i++) {
+			for (int j = 0; j < A[0].length; j++) {
+				for (int k = 0;  k < B[0].length; k++) {
+					for (int l = 0; l < B.length; l++) {
+						if(j==l && k==i) {
+							
+						}
+					}
+					
+				}
+			}
+		}
+
+		
+		
     }
 
 }
