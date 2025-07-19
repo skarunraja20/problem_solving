e34c861f60e6ab71aff51cb9778ae87de7ada92c
skarunraja20 <skarunraja@gmail.com>
2025-07-19 10:57:24 +0530

    adding heaps logic

---
+++ src/com/example/Heaps/Magician_and_chocolates.java
@@ -0,0 +1,11 @@
+package com.example.Heaps;
+
+public class Magician_and_chocolates {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Magician_and_chocolates m = new Magician_and_chocolates();
+
+    }
+
+}
